ae1cb0720587550e510f455832a664b0d8588e6c
fretboarder <cini01@gmail.com>
2018-11-05 17:31:08 +0100

    add DELETE /todos/:id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -106,3 +106,29 @@ describe('GET /todos/:id', () => {
           .end(done)
     })
 })
+
+describe('DELETE /todos/:id', () => {
+    it('should delete todo doc', (done) => {
+        request(app)
+          .delete('/todos/' + dummyTodos[0]._id.toHexString())
+          .expect(200)
+          .expect((res) => {
+              expect(res.body.todo.text).toBe(dummyTodos[0].text)
+          })
+          .end(done)
+    })
+
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+          .delete('/todos/' + (new ObjectID()).toHexString())
+          .expect(404)
+          .end(done)
+    })
+
+    it('should return 404 if non-object ID', (done) => {
+        request(app)
+          .delete('/todos/123')
+          .expect(404)
+          .end(done)
+    })    
+})

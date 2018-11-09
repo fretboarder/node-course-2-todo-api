609fdb92a10058b30565531ec74ce4a585912144
fretboarder <speedskating@ymail.com>
2018-11-09 16:50:32 +0100

    use external config.json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,10 @@
 const env = process.env.NODE_ENV || 'development'
 console.log('env ****', env)
 
-if (env === 'development') {
-    process.env.PORT = 3000
-    process.env.MONGODB_URI =  'mongodb://localhost:27017/TodoApp'
-} else if (env === 'test') {
-    process.env.PORT = 3000
-    process.env.MONGODB_URI =  'mongodb://localhost:27017/TodoAppTest'
+if (env === 'development' || env === 'test') {
+    const config = require('./config.json')
+
+    Object.keys(config[env]).forEach((key) => {
+        process.env[key] = config[env][key]
+    })
 }
